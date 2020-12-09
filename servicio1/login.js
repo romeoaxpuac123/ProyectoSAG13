e8085655d1fe623955a2699da53241627540e62c
bayronaxpuac95 <48611884+romeoaxpuac123@users.noreply.github.com>
2020-12-08 18:17:11 -0600

    Update login.js

--- servicio1/login.js
+++ servicio1/login.js
@@ -1,17 +1,15 @@
 var express = require('express');
 var app = express();
 var mysql = require('mysql');
-const cors = require('cors');
+//const cors = require('cors');
 var path = require('path');
 //host : '172.18.0.2',
 var conexion= mysql.createConnection({
-    host : 'localhost',
-    database : 'Practica1',
+    host : '172.22.0.2',
+    database : 'ProyectoSA',
     user : 'root',
-    password : '1234',
+    password : 'grupo13',
     port: 3306
-
-
 });
 
 conexion.connect(function(err) {
@@ -27,7 +25,7 @@ app.set('port',3003);
 app.set('json spaces',2);
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cors());
+//app.use(cors());
 
 app.get('/auth',(req,res)=>{
 	let Respuesta = {
@@ -37,23 +35,26 @@ app.get('/auth',(req,res)=>{
 });
 
 
-app.post('/auth',(req,res)=>{
+app.post('/authProveedor',(req,res)=>{
 	const {email, pass} = req.body;
 	if(email && pass){
 		console.log("Se busca->" + email);
 		console.log("password->" + pass)
 		
-		conexion.query('SELECT * FROM Cliente WHERE correo = ? AND Password = ?', [email, pass], function(error, result, fields) {
+		conexion.query('SELECT * FROM Proveedor WHERE email = ? AND password = ?', [email, pass], function(error, result, fields) {
 			if (result.length > 0) {
 				
 				const myString = JSON.stringify(result);
 				var resultados = myString.split(',');
 				var user1 =  parseInt(resultados[0].split(':')[1]);
 				var nombre = resultados[1].split(':')[1].replace('\"','').replace('\"','');
-				var tips = resultados[4].split(':')[1].replace('\"','').replace('\"}]','');			
-				res.json({"msg":true,"tipo":tips,"user":user1,"name":nombre});
+				//console.log("RRR*****->" + resultados);
+
+				res.json({"msg":true,"user":user1,"name":nombre});
+				
 			} else {
-				res.json({"msg":false,"tipo":"error","user":0,"name":"error"});
+				res.json({"msg":false,"user":0,"name":"error"});
+				//res.json({"msg":false});
 			}			
 		});
 	}else{
@@ -61,26 +62,79 @@ app.post('/auth',(req,res)=>{
 	}
 });
 
-app.post('/regis',(req,res)=>{
-	const {nombre, email, pass,tipo} = req.body;
-	if(nombre && email && pass && tipo){
-		conexion.query('SELECT * FROM Cliente WHERE correo = ? ', [email], function(error, result, fields) {
+app.post('/authCliente',(req,res)=>{
+	const {email, pass} = req.body;
+	if(email && pass){
+		console.log("Se busca->" + email);
+		console.log("password->" + pass)
+		
+		conexion.query('SELECT * FROM Cliente WHERE email = ? AND password = ?', [email, pass], function(error, result, fields) {
+			if (result.length > 0) {
+				
+				const myString = JSON.stringify(result);
+				var resultados = myString.split(',');
+				var user1 =  parseInt(resultados[0].split(':')[1]);
+				var nombre = resultados[1].split(':')[1].replace('\"','').replace('\"','');
+				//console.log("RRR*****->" + resultados);
+
+				res.json({"msg":true,"user":user1,"name":nombre});
+				
+				res.json({"msg":true});
+			} else {
+				res.json({"msg":false,"user":0,"name":"error"});
+				//res.json({"msg":false});
+			}			
+		});
+	}else{
+		res.json({"msg":false,"tipo":"error","user":0,"name":"error"});
+	}
+});
+
+app.post('/regisProveedor',(req,res)=>{
+	const {empresa, email, password,direccion} = req.body;
+	if(empresa && email && password && direccion){
+		conexion.query('SELECT * FROM Proveedor WHERE email = ? ', [email], function(error, result, fields) {
 			if (result.length > 0) {
 				res.json({"msg":false,"tipo":"error","user":0,"name":"error"});				
 			} else {
-				conexion.query('insert into Cliente (Nombre,Password,correo,tipo) Values (?,?,?,?)',[nombre, pass,email,tipo]);
+				console.log("msg////////////////////////////");
+				conexion.query('INSERT INTO Proveedor (empresa,email,password,direccion) VALUES (?,?,?,?)',[empresa,email,password, direccion]);
 				let elnuevouser = 0;
-				conexion.query('SELECT * FROM Cliente WHERE correo = ? ', [email], function(error, result, fields) {
+				conexion.query('SELECT * FROM Proveedor WHERE email = ? ', [email], function(error, result, fields) {
 					const myString = JSON.stringify(result);
 					var resultados = myString.split(',');
 					elnuevouser =  parseInt(resultados[0].split(':')[1]);
-					res.json({"msg":true,"tipo":tipo,"user":elnuevouser,"name":nombre});
+					res.json({"msg":true,"user":elnuevouser,"name":empresa});
 				});
 				
 			}			
 		});
 	}else{
-		res.json({"msg":false,"tipo":"error","user":0,"name":"error"});
+		res.json({"msg":false,"user":0,"name":"error"});
+	}
+});
+
+app.post('/regisCliente',(req,res)=>{
+	const {Nombre, Apellido, password,email,celular,fotografia} = req.body;
+	if(Nombre && Apellido && password && email && celular && fotografia){
+		conexion.query('SELECT * FROM Cliente WHERE email = ? ', [email], function(error, result, fields) {
+			if (result.length > 0) {
+				res.json({"msg":false,"tipo":"error","user":0,"name":"error"});				
+			} else {
+				console.log("msg////////////////////////////");
+				conexion.query('INSERT INTO Cliente (Nombre,Apellido,password,email,celular,fotografia)VALUES(?,?,?,?,?,?)',[Nombre, Apellido, password,email,celular,fotografia]);
+				let elnuevouser = 0;
+				conexion.query('SELECT * FROM Cliente WHERE email = ? ', [email], function(error, result, fields) {
+					const myString = JSON.stringify(result);
+					var resultados = myString.split(',');
+					elnuevouser =  parseInt(resultados[0].split(':')[1]);
+					res.json({"msg":true,"user":elnuevouser,"name":Nombre});
+				});
+				
+			}			
+		});
+	}else{
+		res.json({"msg":false,"user":0,"name":"error"});
 	}
 });
 
