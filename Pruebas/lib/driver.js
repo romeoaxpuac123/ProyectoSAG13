ff838d42b82b8f4cfa1cd4ddc1b3daaff33ebde0
Jonnathan Castillo <jcastillo@homeland.com.gt>
2020-12-11 00:17:18 -0600

    Pruebas Unitarias

--- Pruebas/lib/driver.js
+++ Pruebas/lib/driver.js
@@ -8,9 +8,11 @@ chai.use(chaiHttp);
 const url = 'http://35.193.123.113:4003/';
 
 describe('Pruebas correctas a servicio 2: ', () => {
+
     it('Obtener todos los productos', (done) => {
         chai.request(url)
-            .get('/MostrarProductos')
+            .post('MostrarProductos')
+            .send({ id: 1 })
             .end(function (err, res) {
                 expect(res).to.have.status(200);
                 done();
@@ -19,10 +21,10 @@ describe('Pruebas correctas a servicio 2: ', () => {
 
     it('Insertar un producto', (done) => {
         chai.request(url)
-            .post('/RegistrarProducto')
-            .send({ nombre: "Hamburguesa",Precio_venta: "15.00",stock:"5", categoria: "Comida", 
-                     imagen: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.hogar.mapfre.es%2Fmedia%2F2018%2F09%2Fhamburguesa-sencilla.jpg&imgrefurl=https%3A%2F%2Fwww.hogar.mapfre.es%2Fcocina%2Frecetas%2Fcarnes%2Fhamburguesa-sencilla%2F&tbnid=fSa-b9YO8FPkFM&vet=12ahUKEwiMh9L21pnsAhUOTlMKHVXFC1UQMygFegUIARDvAQ..i&docid=uDWBipZkf93RBM&w=1104&h=704&q=hamburguesas&ved=2ahUKEwiMh9L21pnsAhUOTlMKHVXFC1UQMygFegUIARDvAQ" ,
-                     id_proveedor:"1" })
+            .post('RegistrarProducto')
+            .send({ nombre: "Camisa",Precio_venta: "15.00",stock:"5", categoria: "Comida", 
+                     imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/245px-NCI_Visuals_Food_Hamburger.jpg" ,
+                     id_provedor:"1" })
             .end(function (err, res) {
                 expect(res).to.have.status(200);
                 done();
@@ -34,16 +36,18 @@ describe('Pruebas correctas a servicio 2: ', () => {
 describe('Pruebas incorrectas a servicio 2: ', () => {
     it('Obtener producto inexistente', (done) => {
         chai.request(url)
-            .get('/urlinventada')
+            .post('urlinventada')
+            .send({ id: "1" })
             .end(function (err, res) {
                 expect(res).to.have.status(404);
                 done();
             });
     });
 
-    it('Borrar producto inexistente (-1)', (done) => {
+    it('Borrar producto inexistente (1582155)', (done) => {
         chai.request(url)
-            .get('/delete?id=-1')
+            .post('/delete')
+            .send({ id: "1582155" })
             .end(function (err, res) {
                 expect(res).to.have.status(404);
                 done();
@@ -51,7 +55,6 @@ describe('Pruebas incorrectas a servicio 2: ', () => {
     });
 
 });
-
 
 
 
