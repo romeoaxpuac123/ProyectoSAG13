b73ef14d7817f36b0caf2367344911a84683d47f
Jonnathan Castillo <jcastillo@homeland.com.gt>
2020-12-12 11:31:08 -0600

    Pruebas Funcionales

---
+++ Pruebas_Funcionales/lib/driver.js
@@ -0,0 +1,36 @@
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+
+
+chai.use(chaiHttp);
+const url = 'http://35.193.123.113:3003/';
+
+describe('Pruebas Funcionales: ', () => {
+
+    it('Registro de Cliente', (done) => {
+        chai.request(url)
+            .post('regisCliente')
+            .send({ Nombre: "Jonnathan",Apellido: "Castillo",password:"56789", email: "jon@gmail.com", 
+            celular: "46877446" ,fotografia:"No foto"  })
+            .end(function (err, res) {
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+
+    it('Login', (done) => {
+        chai.request(url)
+            .post('authCliente')
+            .send({ email: "romeo@gmail.com",pass: "1234" })
+            .end(function (err, res) {
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+
+});
+
+
+
